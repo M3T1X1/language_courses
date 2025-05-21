2e385a2348ea84a04ce107904174b185836e54c8
RC131413 <rcebula123@gamil.com>
2025-05-21 15:19:41 +0200

    remove add cient button

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,38 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\TransakcjeController;
+use App\Http\Controllers\KlientController;
+use App\Http\Controllers\DashboardController;
+
+
+Route::get('/admin/transakcje', [AdminController::class, 'showTransactions'])->name('admin.transakcje');
+Route::get('/transakcje', [TransakcjeController::class, 'index'])->name('transakcje');
+Route::get('/klienci', [KlientController::class, 'index'])->name('klienci.index');
+
+
+Route::resource('klienci', KlientController::class);
+
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', function () {
+    return view('home'); // home.blade.php
+});
+
+Route::get('/login', function () {
+    return view('login'); // login.blade.php
+});
+
+Route::get('/app', function () {
+    return view('app'); // app.blade.php
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard'); // dashboard.blade.php
+});
 Route::get('/login', function () {
     return view('auth.login');
 });
